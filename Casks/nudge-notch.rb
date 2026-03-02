bab1a58cc18cfe999835a7c40501862bd39d4ad5
suryanshkushwaha <suryanshkushwaha@u.nus.edu>
2026-03-03 02:06:30 +0800

    Update nudge-notch.rb

--- Casks/nudge-notch.rb
+++ Casks/nudge-notch.rb
@@ -2,7 +2,7 @@ cask "nudge-notch" do
   version "0.3.7"
   sha256 "99aac618c079626c8e46ffebf638ee1f8f06814673fd65fdc894f41cecbe30d2"
 
-  url "https://github.com/suryanshkushwaha/nudge.notch/releases/download/v\#{version}/nudgeNotch.dmg"
+  url "https://github.com/suryanshkushwaha/nudge.notch/releases/download/v#{version}/nudgeNotch.dmg"
   name "NudgeNotch"
   desc "Gentle nudges for your wellbeing and focus, right in your MacBook notch"
   homepage "https://github.com/suryanshkushwaha/nudge.notch"
